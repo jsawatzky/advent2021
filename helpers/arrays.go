c1f34bc083953d6e54410d195bd465c9e1a12391
Jacob Sawatzky <jsawatzky@outlook.com>
2021-12-11 13:08:03 -0500

    Add array helpers

--- helpers/arrays.go
+++ helpers/arrays.go
@@ -1,1 +1,46 @@
 package helpers
+
+func ForEachNeighborInt(i, j, maxI, maxJ int, diag bool, f func(int, int)) {
+	if i > 0 && j > 0 && diag {
+		f(i-1, j-1)
+	}
+	if i > 0 {
+		f(i-1, j)
+	}
+	if i > 0 && j+1 < maxJ && diag {
+		f(i-1, j+1)
+	}
+	if j+1 < maxJ {
+		f(i, j+1)
+	}
+	if i+1 < maxI && j+1 < maxJ && diag {
+		f(i+1, j+1)
+	}
+	if i+1 < maxI {
+		f(i+1, j)
+	}
+	if i+1 < maxI && j > 0 && diag {
+		f(i+1, j-1)
+	}
+	if j > 0 {
+		f(i, j-1)
+	}
+}
+
+func InInt(v int, arr []int) bool {
+	for _, v2 := range arr {
+		if v == v2 {
+			return true
+		}
+	}
+	return false
+}
+
+func InStr(v rune, s string) bool {
+	for _, v2 := range s {
+		if v == v2 {
+			return true
+		}
+	}
+	return false
+}
